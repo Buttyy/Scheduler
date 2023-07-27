ddbb75402b3cebca8d7cb0f9f4356d6b15d9cddd
Buttyy <syota.bucchi@icloud.com>
2023-07-27 13:30:33 +0900

    init

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "scheduler.h"
+
+int main(void)
+{
+    int process;
+
+    printf("---Scheduler---\n");
+    printf("タスク数: ");
+    scanf("%d", &process);
+}
---
+++ scheduler.h
@@ -0,0 +1,11 @@
+#ifndef SCHEDULER_H_
+#define SCHEDULER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Queue{
+    int progress;
+} Queue;
+
+#endif /* SCHEDULER_H_ */
